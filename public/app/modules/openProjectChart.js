825cace161d5073081632c2331c87660842a4f93
Gavin Li <gavin@noosh.com>
2015-09-22 21:53:50 +0800

    implement chart

--- public/app/modules/openProjectChart.js
+++ public/app/modules/openProjectChart.js
@@ -2,15 +2,39 @@
  * Created by Gavin Li on 9/21/2015.
  */
 ;define(function(require){
+
     /**
      * appView
      */
     var appView = Backbone.View.extend({
         initialize:function(){
+            google.setOnLoadCallback($.proxy(this.drawChart,this));
             this.render();
         },
         render:function(){
-            this.$el.append("test--panel body");
+            this.$el.append("<div style='height: 300px' class='chart-cot'></div>");
+        },
+        drawChart: function(){
+            var data = google.visualization.arrayToDataTable([
+                ['Project Status', 'Number'],
+                ['New',     11],
+                ['In Estimation',      2],
+                ['Quoted',  2],
+                ['Ordered', 2],
+                ['Completed',    7]
+            ]);
+
+            var options = {
+                colors: ['#325bad', '#517ACC', '#739cee', '#94bdff', '#9db9ef'],
+                pieHole: 0.4,
+                animation:{
+                    duration: 1000,
+                    easing: 'out',
+                }
+            };
+
+            var chart = new google.visualization.PieChart(this.$(".chart-cot")[0]);
+            chart.draw(data, options);
         }
     });
     return {
--- public/app/modules/projectOwnerChart.js
+++ public/app/modules/projectOwnerChart.js
@@ -1,20 +1,44 @@
 /**
  * Created by Gavin Li on 9/21/2015.
  */
-;define(function(require){
+;
+define(function (require) {
     /**
      * appView
      */
     var appView = Backbone.View.extend({
-        initialize:function(){
+        initialize: function () {
+            google.setOnLoadCallback($.proxy(this.drawChart,this));
             this.render();
         },
-        render:function(){
-            this.$el.append("test--panel body");
+        render: function () {
+            this.$el.append("<div style='height: 300px' class='chart-cot'></div>");
+        },
+        drawChart: function () {
+            var data2 = google.visualization.arrayToDataTable([
+                ['Project Owner', 'Number'],
+                ['Yang',    3],
+                ['Song',      2],
+                ['Raymond',  2],
+                ['Hung', 2]
+            ]);
+
+            var options = {
+                colors: ['#325bad', '#517ACC', '#739cee', '#94bdff', '#9db9ef'],
+                pieHole: 0.4,
+                animation:{
+                    duration: 1000,
+                    easing: 'out',
+                }
+            };
+
+
+            var chart = new google.visualization.PieChart(this.$(".chart-cot")[0]);
+            chart.draw(data2, options);
         }
     });
     return {
-        init:function(args){
+        init: function (args) {
             return new appView(args);
         }
     }
