fa7f262b5634cc9efee22830c50bbfc6f8e63933
Song Li <lisong@gmail.com>
2015-09-29 15:50:39 -0700

    different chart numbers

--- public/app/modules/mainCharts.js
+++ public/app/modules/mainCharts.js
@@ -15,7 +15,9 @@
             this.$el.append('<div class="panel-chart panel-one-third-area panel-chart1"></div><div class="panel-chart panel-one-third-area panel-chart2"></div><div class="panel-chart panel-one-third-area panel-chart3"></div>')
         },
         drawChart: function(){
-            var data = google.visualization.arrayToDataTable([
+            var data, options, chart1, chart2, chart3;
+
+            data = google.visualization.arrayToDataTable([
                 ['Project Status', 'Number'],
                 ['New',     11],
                 ['In Estimation',      2],
@@ -23,8 +25,7 @@
                 ['Ordered', 2],
                 ['Completed',    7]
             ]);
-
-            var options = {
+            options = {
                 colors: ['#325bad', '#517ACC', '#739cee', '#94bdff', '#9db9ef'],
                 pieHole: 0.4,
                 animation:{
@@ -32,20 +33,61 @@
                     easing: 'out'
                 },
                 chartArea: {
-                    left: 10,
+                    left: 50,
                     top: 10,
                     width: '85%',
                     height: '85%'
                 }
             };
-
-            var chart1 = new google.visualization.PieChart(this.$(".panel-chart1")[0]);
+            chart1 = new google.visualization.PieChart(this.$(".panel-chart1")[0]);
             chart1.draw(data, options);
 
-            var chart2 = new google.visualization.PieChart(this.$(".panel-chart2")[0]);
+            data = google.visualization.arrayToDataTable([
+                ['Breakdown by Owner', 'Number'],
+                ['Ron Nash',     12],
+                ['Ken Liao',      18],
+                ['Figo Fei',  8],
+                ['Dev Singh', 22]
+            ]);
+            options = {
+                colors: ['#325bad', '#517ACC', '#739cee', '#94bdff', '#9db9ef'],
+                pieHole: 0.4,
+                animation:{
+                    duration: 1000,
+                    easing: 'out'
+                },
+                chartArea: {
+                    left: 50,
+                    top: 10,
+                    width: '85%',
+                    height: '85%'
+                }
+            };
+            chart2 = new google.visualization.PieChart(this.$(".panel-chart2")[0]);
             chart2.draw(data, options);
 
-            var chart3 = new google.visualization.PieChart(this.$(".panel-chart3")[0]);
+            data = google.visualization.arrayToDataTable([
+                ['Projects By Creator', 'Number'],
+                ['Ron Nash',     23],
+                ['Ken Liao',      8],
+                ['Figo Fei',  19],
+                ['Dev Singh', 32]
+            ]);
+            options = {
+                colors: ['#325bad', '#517ACC', '#739cee', '#94bdff', '#9db9ef'],
+                pieHole: 0.4,
+                animation:{
+                    duration: 1000,
+                    easing: 'out'
+                },
+                chartArea: {
+                    left: 50,
+                    top: 10,
+                    width: '85%',
+                    height: '85%'
+                }
+            };
+            chart3 = new google.visualization.PieChart(this.$(".panel-chart3")[0]);
             chart3.draw(data, options);
         }
     });
